af82af137f9e61282ec22e9543b07254d2949a88
Frowing33 <jean.comberton@outlook.fr>
2024-01-23 09:39:02 +0100

    architecture site

---
+++ components/layout.js
@@ -0,0 +1,18 @@
+// Layout.js
+import React from 'react';
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <header>
+        {/* Header content */}
+      </header>
+      <main>{children}</main>
+      <footer>
+        {/* Footer content */}
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+import React from 'react';
+import Layout from '../components/layout';
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>Welcome to the Event Platform</h1>
+      {/* Other content */}
+    </Layout>
+  );
+};
+
+export default Home;
